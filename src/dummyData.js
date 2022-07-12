4a99cb8ffb2259fcbf4d8e13d77945e61e145d38
Gehini Chandrika Kummarakuntla <63400885+Chandrika1302@users.noreply.github.com>
2022-07-12 19:26:22 +0530

    Add files via upload

---
+++ src/dummyData.js
@@ -0,0 +1,86 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "assets/person/1.jpg",
+    username: "Vjay",
+  },
+  {
+    id:2,
+    profilePicture: "assets/person/2.jpeg",
+    username: "NTR",
+  },
+  {
+    id:3,
+    profilePicture: "assets/person/3.jpg",
+    username: "Ninatara",
+  },
+  {
+    id:4,
+    profilePicture: "assets/person/4.jpeg",
+    username: "Arjun",
+  },
+  {
+    id:5,
+    profilePicture: "assets/person/5.jpeg",
+    username: "Pradep",
+  },
+  {
+    id:6,
+    profilePicture: "assets/person/6.jpg",
+    username: "Akhil",
+  },
+  
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo: "assets/post/1.jpg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    photo: "assets/post/2.jpg",
+    date: "15 mins ago",
+    userId: 2,
+    like: 2,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: "Every moment is a fresh beginning.",
+    photo: "assets/post/3.jpeg",
+    date: "1 hour ago",
+    userId: 3,
+    like: 61,
+    comment: 2,
+  },
+  {
+    id: 4,
+    photo: "assets/post/4.jpeg",
+    date: "4 hours ago",
+    userId: 4,
+    like: 7,
+    comment: 3,
+  },
+  {
+    id: 5,
+    photo: "assets/post/5.jpeg",
+    date: "5 hours ago",
+    userId: 5,
+    like: 23,
+    comment: 5,
+  },
+  {
+    id: 6,
+    photo: "assets/post/7.jpg",
+    date: "1 day ago",
+    userId: 6,
+    like: 44,
+    comment: 6,
+  },
+];
